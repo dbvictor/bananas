1d5ae026afe4c1215db4179df3ab58da7cee9e61
dbvictor <dbvictor@live.com>
2014-09-28 15:10:47 -0700

    merge best of existing projects setup (manifest, drawable, libs, app, parse)

--- gen/com/yahoo/bananas/R.java
+++ gen/com/yahoo/bananas/R.java
@@ -11,7 +11,15 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_create=0x7f020000;
+        public static final int ic_action_internet_off=0x7f020001;
+        public static final int ic_action_internet_on=0x7f020002;
+        public static final int ic_action_profile=0x7f020003;
+        public static final int ic_action_settings=0x7f020004;
+        public static final int ic_action_share=0x7f020005;
+        public static final int ic_home=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
+        public static final int ic_mentions=0x7f020008;
     }
     public static final class layout {
         public static final int activity_joke_stream=0x7f030000;

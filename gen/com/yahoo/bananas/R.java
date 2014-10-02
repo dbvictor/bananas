3899447a777a40ddcbd214fa398c6a7fdba081f8
makhija <makhija@yahoo-inc.com>
2014-10-02 00:23:22 -0700

    Actually committing

--- gen/com/yahoo/bananas/R.java
+++ gen/com/yahoo/bananas/R.java
@@ -10,11 +10,15 @@ package com.yahoo.bananas;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int darkyellow=0x7f040001;
+        public static final int yellow=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_action_create=0x7f020000;
@@ -28,32 +32,32 @@ public final class R {
         public static final int ic_mentions=0x7f020008;
     }
     public static final class id {
-        public static final int actionCreate=0x7f080017;
-        public static final int actionInternetToggle=0x7f080019;
-        public static final int actionProfile=0x7f080018;
-        public static final int action_settings=0x7f080016;
-        public static final int bLogin=0x7f080004;
-        public static final int bSkip=0x7f080005;
-        public static final int bSubmit=0x7f080001;
-        public static final int etNewJoke=0x7f080000;
-        public static final int flContainer=0x7f080003;
-        public static final int flProfileJokeStreamContainer=0x7f08000d;
-        public static final int ivProfileImage=0x7f080007;
-        public static final int llCounts=0x7f08000a;
-        public static final int lvJokes=0x7f08000f;
-        public static final int rlHeader=0x7f080006;
-        public static final int swipeContainer=0x7f08000e;
-        public static final int tvBody=0x7f080012;
-        public static final int tvCharsRemaining=0x7f080002;
-        public static final int tvDownVotes=0x7f080014;
-        public static final int tvFollowers=0x7f08000b;
-        public static final int tvFollowing=0x7f08000c;
-        public static final int tvRealName=0x7f080008;
-        public static final int tvShares=0x7f080015;
-        public static final int tvTagline=0x7f080009;
-        public static final int tvTime=0x7f080011;
-        public static final int tvUpVotes=0x7f080013;
-        public static final int tvUserName=0x7f080010;
+        public static final int actionCreate=0x7f090017;
+        public static final int actionInternetToggle=0x7f090019;
+        public static final int actionProfile=0x7f090018;
+        public static final int action_settings=0x7f090016;
+        public static final int bLogin=0x7f090004;
+        public static final int bSkip=0x7f090005;
+        public static final int bSubmit=0x7f090001;
+        public static final int etNewJoke=0x7f090000;
+        public static final int flContainer=0x7f090003;
+        public static final int flProfileJokeStreamContainer=0x7f09000d;
+        public static final int ivProfileImage=0x7f090007;
+        public static final int llCounts=0x7f09000a;
+        public static final int lvJokes=0x7f09000f;
+        public static final int rlHeader=0x7f090006;
+        public static final int swipeContainer=0x7f09000e;
+        public static final int tvBody=0x7f090012;
+        public static final int tvCharsRemaining=0x7f090002;
+        public static final int tvDownVotes=0x7f090014;
+        public static final int tvFollowers=0x7f09000b;
+        public static final int tvFollowing=0x7f09000c;
+        public static final int tvRealName=0x7f090008;
+        public static final int tvShares=0x7f090015;
+        public static final int tvTagline=0x7f090009;
+        public static final int tvTime=0x7f090011;
+        public static final int tvUpVotes=0x7f090013;
+        public static final int tvUserName=0x7f090010;
     }
     public static final class layout {
         public static final int activity_create=0x7f030000;
@@ -65,26 +69,26 @@ public final class R {
         public static final int joke_item=0x7f030006;
     }
     public static final class menu {
-        public static final int login=0x7f070000;
-        public static final int menu_timeline=0x7f070001;
-        public static final int photos=0x7f070002;
+        public static final int login=0x7f080000;
+        public static final int menu_timeline=0x7f080001;
+        public static final int photos=0x7f080002;
     }
     public static final class string {
-        public static final int action_create=0x7f050002;
-        public static final int action_internet_toggle=0x7f050003;
-        public static final int action_profile=0x7f050004;
-        public static final int action_settings=0x7f050005;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
-        public static final int login_skip_button=0x7f05000e;
-        public static final int login_twitter_button=0x7f050007;
-        public static final int new_joke_button=0x7f05000c;
-        public static final int new_joke_hint=0x7f05000b;
-        public static final int title_activity_create=0x7f050009;
-        public static final int title_activity_detail=0x7f05000d;
-        public static final int title_activity_photos=0x7f050006;
-        public static final int title_activity_profile=0x7f05000a;
-        public static final int title_activity_timeline=0x7f050008;
+        public static final int action_create=0x7f060002;
+        public static final int action_internet_toggle=0x7f060003;
+        public static final int action_profile=0x7f060004;
+        public static final int action_settings=0x7f060005;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060001;
+        public static final int login_skip_button=0x7f06000e;
+        public static final int login_twitter_button=0x7f060007;
+        public static final int new_joke_button=0x7f06000c;
+        public static final int new_joke_hint=0x7f06000b;
+        public static final int title_activity_create=0x7f060009;
+        public static final int title_activity_detail=0x7f06000d;
+        public static final int title_activity_photos=0x7f060006;
+        public static final int title_activity_profile=0x7f06000a;
+        public static final int title_activity_timeline=0x7f060008;
     }
     public static final class style {
         /** 
@@ -108,10 +112,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }

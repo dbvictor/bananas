cabaeb31f221b6dff78aab5c0a4c3b2538d2fcc3
makhija <makhija@yahoo-inc.com>
2014-10-04 17:53:34 -0700

    Shortening the created at display

--- src/com/yahoo/bananas/util/Util.java
+++ src/com/yahoo/bananas/util/Util.java
@@ -9,6 +9,20 @@ import android.text.format.DateUtils;
 
 
 public class Util {
+	
+	private static final String H = "h ago";
+	private static final String M = "m ago";
+	private static final String S = "s ago";
+	private static final String D = "d ago";
+	private static final String DAYS_AGO = " days ago";
+	private static final String DAY_AGO = " day ago";
+	private static final String HOURS_AGO = " hours ago";
+	private static final String HOUR_AGO = " hour ago";
+	private static final String MINUTES_AGO = " minutes ago";
+	private static final String MINUTE_AGO = " minute ago";
+	private static final String SECONDS_AGO = " seconds ago";
+	private static final String SECOND_AGO = " second ago";
+	
 	// getRelativeTimeAgo("Mon Apr 01 21:16:23 +0000 2014");
 	public static String getRelativeTimeAgo(String rawJsonDate) {
 		String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
@@ -23,6 +37,15 @@ public class Util {
 		} catch (java.text.ParseException e) {
 			e.printStackTrace();
 		}
+		
+		relativeDate = relativeDate.replace(SECOND_AGO, S);
+		relativeDate = relativeDate.replace(SECONDS_AGO, S);
+		relativeDate = relativeDate.replace(MINUTE_AGO, M);
+		relativeDate = relativeDate.replace(MINUTES_AGO, M);
+		relativeDate = relativeDate.replace(HOUR_AGO, H);
+		relativeDate = relativeDate.replace(HOURS_AGO, H);
+		relativeDate = relativeDate.replace(DAY_AGO, D);
+		relativeDate = relativeDate.replace(DAYS_AGO, D);
 
 		return relativeDate;
 	}

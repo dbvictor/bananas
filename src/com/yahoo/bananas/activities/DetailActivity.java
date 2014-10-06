af0e1a1c9495334eee590ce763c87c0578f20beb
makhija <makhija@yahoo-inc.com>
2014-10-05 23:40:33 -0700

    Removing everything from the detail view that was already accomodated well on the stream and adding the joke creation user profile image, the user name, and a button to view more jokes from the same user without doing anything for now and just being a placeholder. yet to add a like dislike ability to the page which will mostly come above more from this user

--- src/com/yahoo/bananas/activities/DetailActivity.java
+++ src/com/yahoo/bananas/activities/DetailActivity.java
@@ -1,17 +1,17 @@
 package com.yahoo.bananas.activities;
 
-import java.util.Date;
-
 import android.app.Activity;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.nostra13.universalimageloader.core.ImageLoader;
 import com.yahoo.bananas.R;
 import com.yahoo.bananas.models.Joke;
-import com.yahoo.bananas.util.Util;
 
 public class DetailActivity extends Activity {
 
@@ -30,13 +30,17 @@ public class DetailActivity extends Activity {
 		tvBody.setText(joke.getText());
 		TextView tvJokeTitle = (TextView)findViewById(R.id.tvJokeTitle);
 		tvJokeTitle.setText(joke.getTitle());
-		TextView tvCreatedAt = (TextView) findViewById(R.id.tvCreatedAt);
-		Date createdAt = joke.getCreatedAt();
-		if (createdAt != null) {
-			tvCreatedAt.setText(Util.getRelativeTimeAgo(createdAt.toString()));
-		}
-		TextView tvCategory = (TextView) findViewById(R.id.tvCategory);
-		tvCategory.setText(joke.getCategory().getDisplayName());
+		TextView tvCreationUser = (TextView) findViewById(R.id.tvCreationUser);
+		tvCreationUser.setText(joke.getCreatedByUser().getRealName());
+		ImageView ivCreationUser = (ImageView)findViewById(R.id.ivCreationUser);
+		ImageLoader.getInstance().displayImage(joke.getCreatedByUser().getImageUrl(), ivCreationUser);
+//		TextView tvCreatedAt = (TextView) findViewById(R.id.tvCreatedAt);
+//		Date createdAt = joke.getCreatedAt();
+//		if (createdAt != null) {
+//			tvCreatedAt.setText(Util.getRelativeTimeAgo(createdAt.toString()));
+//		}
+//		TextView tvCategory = (TextView) findViewById(R.id.tvCategory);
+//		tvCategory.setText(joke.getCategory().getDisplayName());
 		setActionBarColor();
 	}
 	
